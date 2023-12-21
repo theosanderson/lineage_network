02bc6ce89350a425f7e580e7166c1df79aceb831
Theo Sanderson <theo@theo.io>
2023-12-21 14:32:14 +0000

    update

--- app.py
+++ app.py
@@ -6,6 +6,97 @@ import matplotlib.pyplot as plt
 plt.rcParams['svg.fonttype'] = 'none'
 from itertools import combinations
 from io import BytesIO
+import numpy as np
+from scipy.optimize import minimize
+
+
+def get_graph_layout(dists):
+    
+
+
+    # If any pairwise distances are 0 raise an error and say what they are
+    zero_distances = [pair for pair, distance in dists.items() if distance == 0]
+    if zero_distances:
+        st.error(f"The following pairs have a distance of 0: {zero_distances}. A graph cannot be plotted. Please remove one of the pair.")
+
+    G = nx.Graph()
+
+
+    for pair, distance in dists.items():
+        G.add_edge(pair[0], pair[1], weight=distance)
+    
+    # create random initial positions for the nodes
+    
+    initial_pos = nx.random_layout(G)
+
+    pos = get_custom_layout2(G, iterations=100)
+    return pos, G
+
+
+
+def get_custom_layout2(G, iterations=50):
+    """
+    Calculate a custom layout for the nodes in a graph G based on stress minimization.
+    """
+
+    def stress(flat_positions):
+        positions = flat_positions.reshape((len(nodes), 2))
+        stress_value = 0
+        for i in range(len(nodes)):
+            for j in range(i + 1, len(nodes)):
+                distance = np.linalg.norm(positions[i] - positions[j])
+                graph_distance = distances.get((nodes[i], nodes[j]), distances.get((nodes[j], nodes[i]), 0))
+                stress_value += (graph_distance - distance) ** 2
+        return stress_value
+
+    nodes = list(G.nodes())
+    distances = nx.get_edge_attributes(G, 'weight')
+
+    # Initial random positions flattened into a one-dimensional array
+    initial_positions = np.random.rand(len(nodes) * 2)
+
+    # Minimize the stress function
+    result = minimize(stress, initial_positions, method='CG', options={'maxiter': iterations})
+
+    # Reshape the result and create a position dictionary
+    optimized_positions = result.x.reshape((len(nodes), 2))
+    pos = {nodes[i]: optimized_positions[i] for i in range(len(nodes))}
+
+    return pos
+
+def optimize_layout(dists, num_iterations=100):
+    best_correlation = -1
+    best_layout = None
+    best_graph = None
+
+    for _ in range(num_iterations):
+        pos, G = get_graph_layout(dists)
+
+        # Calculate the correlation between graph distances and layout distances
+        correlation , _ , _= calculate_correlation(G, pos)
+        
+        if correlation > best_correlation:
+            print(f"New best correlation: {correlation}")
+            best_correlation = correlation
+            best_layout = pos
+            best_graph = G
+
+    return best_layout, best_graph
+
+def calculate_correlation(G, pos):
+    graph_distances = []
+    layout_distances = []
+
+    for edge in G.edges():
+        node1, node2 = edge
+        graph_distance = G[node1][node2]['weight']
+        layout_distance = np.linalg.norm(np.array(pos[node1]) - np.array(pos[node2]))
+
+        graph_distances.append(graph_distance)
+        layout_distances.append(layout_distance)
+
+    correlation = np.corrcoef(graph_distances, layout_distances)[0, 1]
+    return correlation, graph_distances, layout_distances
 
 # Function to download and load JSON data from URL
 def download_data(url):
@@ -75,26 +166,31 @@ def calculate_all_pairwise_distances(keys, mutation_paths,debug_print):
     return pairwise_distances
 
 # Function to draw network graph
-def draw_graph(mutation_paths, keys, show_edge_labels, mutation_key, file_format='png'):
-    dists = calculate_all_pairwise_distances(keys, mutation_paths, debug_print = enable_site_display and file_format == 'png')
-
-    # If any pairwise distances are 0 raise an error and say what they are
-    zero_distances = [pair for pair, distance in dists.items() if distance == 0]
-    if zero_distances:
-        st.error(f"The following pairs have a distance of 0: {zero_distances}. A graph cannot be plotted. Please remove one of the pair.")
-
-
-    G = nx.Graph()
-    for pair, distance in dists.items():
-        G.add_edge(pair[0], pair[1], weight=distance)
+def draw_graph(pos, G, keys, show_edge_labels, mutation_key, file_format='png'):
 
     plt.figure(figsize=(12, 12))
-    pos = nx.kamada_kawai_layout(G)
-    nx.draw(G, pos, with_labels=True, node_color='skyblue', edge_color='gray', node_size = 2000)
+    plt.tight_layout()
+    #plt.axis('off')
+    plt.gca().set_aspect('equal')
+    plt.gca().set_axis_off()
+    # tight layout
+  
+
+    # Draw nodes
+
+    for key in keys:
+        node_labels = {key: key}
+        nx.draw_networkx_nodes(G, pos, nodelist=[key], node_size=500, node_color='skyblue', label=key)
+        nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=10)
+    
     
     if show_edge_labels:
         edge_labels = nx.get_edge_attributes(G, 'weight')
         nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+    # Draw edges
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+
 
 
     buffer = BytesIO()
@@ -132,10 +228,36 @@ if json_data:
     
     if keys:
 
-        svg_buffer  =draw_graph(mutation_paths, keys, show_edge_labels, mutation_key, file_format='svg')
+        dists = calculate_all_pairwise_distances(keys, mutation_paths, debug_print = enable_site_display)
 
-        buffer = draw_graph(mutation_paths, keys, show_edge_labels, mutation_key)
+        pos, G=  optimize_layout(dists, num_iterations=10)
+       
+        svg_buffer  =draw_graph(pos, G, keys, show_edge_labels, mutation_key, file_format='svg')
+
+        buffer = draw_graph(pos, G, keys, show_edge_labels, mutation_key, file_format='png')
         st.image(buffer)
+
+        # Plot correlation
+        correlation, graph_distances, layout_distances = calculate_correlation(G, pos)
+
+        # scatter
+        fig, ax = plt.subplots(figsize=(5,4), dpi=60)
+        # lowdpi
+
+       
+
+        ax.scatter(graph_distances, layout_distances)
+        ax.set_xlabel('True distance')
+        ax.set_ylabel('Plotted distance')
+        ax.set_title(f'Correlation of distances: {correlation:.2f}')
+        # save to buffer 
+        buffer = BytesIO()
+        fig.savefig(buffer, format='png')
+        buffer.seek(0)
+        st.image(buffer)
+
+
+
 
     
         st.download_button(
